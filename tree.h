eddbbd80a35eff558b6829ef1dfdcc18ff8aabe2
gia-ai <garance.grilikhes@efrei.net>
2024-11-17 16:59:11 +0100

    tree.h

--- tree.h
+++ tree.h
@@ -1,8 +1,21 @@
 //
 // Created by LiamC on 08/11/2024.
 //
-
 #ifndef TREE_H
 #define TREE_H
+
+typedef struct node
+{
+    int x;
+    int y;
+    int orientation;
+    struct node *avance10;
+    struct node *avance20;
+    struct node *avance30;
+    struct node *recule10;
+    struct node *tourneg;
+    struct node *tourned
+    struct node *demitour;
+} node ;
 
 #endif //TREE_H
